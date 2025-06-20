5e7fed1a108cf257f818988db516477d226aaff4
Gedeon Guerra QA Engineer <netoguerra360@gmail.com>
2025-06-20 18:55:39 -0300

    menuPage.js

--- cypress/pages/menuPage.js
+++ cypress/pages/menuPage.js
@@ -1,18 +1,21 @@
 class MenuPage {
 
+    // Retorna um objeto com os seletores utilizados na página de menu.
+    // Centralizar os seletores aqui facilita a manutenção e reutilização futura.
     selectorsList() {
         const selectors = {
-            myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
-
+            myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]', // Botão ou link para acessar a seção "My Info".
         }
 
-        return selectors
+        return selectors;
     }
 
+    // Acessa a seção "My Info" clicando no botão correspondente.
+    // Boa prática encapsular ações de navegação em métodos descritivos como este.
     accessMyInfo() {
-        cy.get(this.selectorsList().myInfoButton).click()
+        cy.get(this.selectorsList().myInfoButton).click(); // Executa o clique no botão "My Info".
     }
 
 }
 
-export default MenuPage
+export default MenuPage;
