aab0da25d0c5764f41a0e64b8fbbc079571dacba
Gedeon Guerra QA Engineer <netoguerra360@gmail.com>
2025-06-20 19:05:08 -0300

    user.spec.cy.js

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -5,8 +5,9 @@ import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
 const Chance = require('chance')
-
 const chance = Chance()
+
+// Instanciando as páginas que serão utilizadas nos testes (Page Object Model).
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -14,18 +15,29 @@ const myInfoPage = new MyInfoPage()
 
 describe('orange HRM Test', () => {
 
-
   it('user Info Update - sucess', () => {
+    // Acessa a página de login do sistema.
     loginPage.accessLoginPage()
+
+    // Realiza login com credenciais válidas (vindas do arquivo de dados de teste).
     loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password)
-    
+
+    // Valida se o dashboard foi carregado corretamente após o login.
     dashboardPage.checkDashboardPage()
 
+    // Navega até a aba "My Info" através do menu lateral.
     menuPage.accessMyInfo()
 
-    myInfoPage.fillPersonalDetails( chance.first(),chance.last())
-    myInfoPage.fillEmployeeDetails('employeeId', 'OtherId', '452.973.123-21',)
+    // Preenche os campos de nome e sobrenome com valores aleatórios gerados pela lib Chance.
+    myInfoPage.fillPersonalDetails(chance.first(), chance.last())
+
+    // Preenche os demais dados do perfil do colaborador (dados genéricos para exemplo).
+    myInfoPage.fillEmployeeDetails('employeeId', 'OtherId', '452.973.123-21')
+
+    // (Placeholder) Método reservado para preenchimento de status do colaborador.
     myInfoPage.fillStatus()
+
+    // Salva as informações e verifica se a operação foi realizada com sucesso.
     myInfoPage.saveFor()
   })
 
